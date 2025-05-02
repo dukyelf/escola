3fcd61da65ff559f0255fbc9cab5a8e2197fd284
matijoy <teteu12212@gmail.com>
2025-05-02 08:31:55 -0300

    Delete matheussql2.sql

--- matheussql2.sql
+++
@@ -1,5 +0,0 @@
-insert into funcionario
-values ('12345678901', 'Matheus da Nóbrega', 52.451),
-('12345678901', 'William Gates', 12.000),
-('12345678902', 'Linus Torvalds', 100.000),
-('12345678903', 'Farrokh Bulsara', 500.000)
